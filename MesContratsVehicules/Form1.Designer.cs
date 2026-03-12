c9e18aef4818c93d8fc2afb8ff992c807c38cfd6
Ryo_off <87469026+Seryomp@users.noreply.github.com>
2026-03-12 16:10:41 +0100

    Ajoutez des fichiers projet.

---
+++ MesContratsVehicules/Form1.Designer.cs
@@ -0,0 +1,531 @@
+﻿namespace MesContratsVehicules
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            groupBox2 = new GroupBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            button2 = new Button();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            textBox9 = new TextBox();
+            label9 = new Label();
+            textBox10 = new TextBox();
+            label10 = new Label();
+            textBox11 = new TextBox();
+            label11 = new Label();
+            groupBox3 = new GroupBox();
+            textBox15 = new TextBox();
+            button3 = new Button();
+            label12 = new Label();
+            label14 = new Label();
+            label15 = new Label();
+            label16 = new Label();
+            label17 = new Label();
+            label18 = new Label();
+            label19 = new Label();
+            textBox12 = new TextBox();
+            textBox13 = new TextBox();
+            textBox14 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            dateTimePicker3 = new DateTimePicker();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(223, 206);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Categorie";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(72, 20);
+            label1.TabIndex = 0;
+            label1.Text = "categorie";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(0, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(83, 20);
+            label2.TabIndex = 1;
+            label2.Text = "description";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 95);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 20);
+            label3.TabIndex = 2;
+            label3.Text = "TarifJour";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(10, 133);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Caution";
+            label4.Click += label4_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(39, 170);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 4;
+            button1.Text = "Valider";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(84, 23);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 27);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(84, 59);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 27);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(84, 95);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 27);
+            textBox3.TabIndex = 7;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(76, 137);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 27);
+            textBox4.TabIndex = 8;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(textBox11);
+            groupBox2.Controls.Add(label11);
+            groupBox2.Controls.Add(textBox10);
+            groupBox2.Controls.Add(label10);
+            groupBox2.Controls.Add(textBox9);
+            groupBox2.Controls.Add(label9);
+            groupBox2.Controls.Add(textBox5);
+            groupBox2.Controls.Add(textBox6);
+            groupBox2.Controls.Add(textBox7);
+            groupBox2.Controls.Add(textBox8);
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(label7);
+            groupBox2.Controls.Add(label8);
+            groupBox2.Location = new Point(544, 12);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(244, 333);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Vehicule";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(107, 137);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(100, 27);
+            textBox5.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(84, 95);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(100, 27);
+            textBox6.TabIndex = 7;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(84, 59);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(100, 27);
+            textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(84, 23);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(100, 27);
+            textBox8.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(90, 298);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 4;
+            button2.Text = "Valider";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(10, 133);
+            label5.Name = "label5";
+            label5.Size = new Size(91, 20);
+            label5.TabIndex = 3;
+            label5.Text = "Kilometrage";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(6, 95);
+            label6.Name = "label6";
+            label6.Size = new Size(60, 20);
+            label6.TabIndex = 2;
+            label6.Text = "Modele";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(0, 59);
+            label7.Name = "label7";
+            label7.Size = new Size(75, 20);
+            label7.TabIndex = 1;
+            label7.Text = "Prestation";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(6, 23);
+            label8.Name = "label8";
+            label8.Size = new Size(51, 20);
+            label8.TabIndex = 0;
+            label8.Text = "Annee";
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(130, 179);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(100, 27);
+            textBox9.TabIndex = 10;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(10, 179);
+            label9.Name = "label9";
+            label9.Size = new Size(114, 20);
+            label9.TabIndex = 9;
+            label9.Text = "Immatriculation";
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(87, 222);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(100, 27);
+            textBox10.TabIndex = 12;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(21, 222);
+            label10.Name = "label10";
+            label10.Size = new Size(60, 20);
+            label10.TabIndex = 11;
+            label10.Text = "Marque";
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(107, 265);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(100, 27);
+            textBox11.TabIndex = 10;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(21, 268);
+            label11.Name = "label11";
+            label11.Size = new Size(74, 20);
+            label11.TabIndex = 9;
+            label11.Text = "Categorie";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(dateTimePicker3);
+            groupBox3.Controls.Add(dateTimePicker2);
+            groupBox3.Controls.Add(dateTimePicker1);
+            groupBox3.Controls.Add(textBox14);
+            groupBox3.Controls.Add(textBox13);
+            groupBox3.Controls.Add(textBox12);
+            groupBox3.Controls.Add(label19);
+            groupBox3.Controls.Add(label18);
+            groupBox3.Controls.Add(label17);
+            groupBox3.Controls.Add(label16);
+            groupBox3.Controls.Add(textBox15);
+            groupBox3.Controls.Add(button3);
+            groupBox3.Controls.Add(label12);
+            groupBox3.Controls.Add(label14);
+            groupBox3.Controls.Add(label15);
+            groupBox3.Location = new Point(22, 234);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(386, 328);
+            groupBox3.TabIndex = 9;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Contrat";
+            // 
+            // textBox15
+            // 
+            textBox15.Location = new Point(84, 23);
+            textBox15.Name = "textBox15";
+            textBox15.Size = new Size(100, 27);
+            textBox15.TabIndex = 5;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(84, 293);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 4;
+            button3.Text = "Valider";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(6, 91);
+            label12.Name = "label12";
+            label12.Size = new Size(93, 20);
+            label12.TabIndex = 3;
+            label12.Text = "Date de tour";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(9, 53);
+            label14.Name = "label14";
+            label14.Size = new Size(94, 20);
+            label14.TabIndex = 1;
+            label14.Text = "Date Contrat";
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(6, 23);
+            label15.Name = "label15";
+            label15.Size = new Size(63, 20);
+            label15.TabIndex = 0;
+            label15.Text = "Numero";
+            // 
+            // label16
+            // 
+            label16.AutoSize = true;
+            label16.Location = new Point(6, 129);
+            label16.Name = "label16";
+            label16.Size = new Size(106, 20);
+            label16.TabIndex = 6;
+            label16.Text = "Date de retour";
+            // 
+            // label17
+            // 
+            label17.AutoSize = true;
+            label17.Location = new Point(22, 162);
+            label17.Name = "label17";
+            label17.Size = new Size(66, 20);
+            label17.TabIndex = 7;
+            label17.Text = "Km Prise";
+            label17.Click += label17_Click;
+            // 
+            // label18
+            // 
+            label18.AutoSize = true;
+            label18.Location = new Point(20, 198);
+            label18.Name = "label18";
+            label18.Size = new Size(79, 20);
+            label18.TabIndex = 8;
+            label18.Text = "Km Retour";
+            label18.Click += label18_Click;
+            // 
+            // label19
+            // 
+            label19.AutoSize = true;
+            label19.Location = new Point(14, 235);
+            label19.Name = "label19";
+            label19.Size = new Size(64, 20);
+            label19.TabIndex = 9;
+            label19.Text = "Vehicule";
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(84, 228);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(100, 27);
+            textBox12.TabIndex = 10;
+            // 
+            // textBox13
+            // 
+            textBox13.Location = new Point(118, 195);
+            textBox13.Name = "textBox13";
+            textBox13.Size = new Size(100, 27);
+            textBox13.TabIndex = 11;
+            // 
+            // textBox14
+            // 
+            textBox14.Location = new Point(113, 162);
+            textBox14.Name = "textBox14";
+            textBox14.Size = new Size(100, 27);
+            textBox14.TabIndex = 12;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(118, 129);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(250, 27);
+            dateTimePicker1.TabIndex = 14;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.Location = new Point(105, 89);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(250, 27);
+            dateTimePicker2.TabIndex = 15;
+            // 
+            // dateTimePicker3
+            // 
+            dateTimePicker3.Location = new Point(105, 56);
+            dateTimePicker3.Name = "dateTimePicker3";
+            dateTimePicker3.Size = new Size(250, 27);
+            dateTimePicker3.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 574);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Menu";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button button1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private GroupBox groupBox2;
+        private TextBox textBox11;
+        private Label label11;
+        private TextBox textBox10;
+        private Label label10;
+        private TextBox textBox9;
+        private Label label9;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Button button2;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private GroupBox groupBox3;
+        private Label label17;
+        private Label label16;
+        private TextBox textBox15;
+        private Button button3;
+        private Label label12;
+        private Label label14;
+        private Label label15;
+        private Label label19;
+        private Label label18;
+        private DateTimePicker dateTimePicker3;
+        private DateTimePicker dateTimePicker2;
+        private DateTimePicker dateTimePicker1;
+        private TextBox textBox14;
+        private TextBox textBox13;
+        private TextBox textBox12;
+    }
+}
---
+++ MesContratsVehicules/Form1.cs
@@ -0,0 +1,55 @@
+namespace MesContratsVehicules
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        
+        }
+    }
+}
